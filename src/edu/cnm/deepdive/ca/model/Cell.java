36bba5d7a46bcfe93828c8f7d0e35d99a944642a
Kevin Simms <ksimms2@cnm.edu>
2019-02-12 09:36:47 -0700

    added JAR artifact

--- src/edu/cnm/deepdive/ca/model/Cell.java
+++ src/edu/cnm/deepdive/ca/model/Cell.java
@@ -4,30 +4,30 @@ public enum Cell {
   DEAD{
     @Override
     public Cell next(Cell[][] terrain, int row, int column) {
-      return (livingNieghors(terrain, row,  column) == 3) ? ALIVE : DEAD;
+      return (livingNeighbors(terrain, row,  column) == 3) ? ALIVE : DEAD;
     }
   },
   ALIVE{
     @Override
     public Cell next(Cell[][] terrain, int row, int column) {
-      int count = livingNieghors(terrain, row, column);
+      int count = livingNeighbors(terrain, row, column);
       return (count == 2 || count == 3) ? ALIVE : DEAD;
     }
 
     @Override
-    protected int livingNieghors(Cell[][] terrain, int row, int column) {
-      return super.livingNieghors(terrain, row, column) - 1;
+    protected int livingNeighbors(Cell[][] terrain, int row, int column) {
+      return super.livingNeighbors(terrain, row, column) - 1;
     }
   };
 
   public abstract Cell next(Cell[][] terrain, int row, int column);
 
-  protected int livingNieghors(Cell[][] terrain, int row, int column){
+  protected int livingNeighbors(Cell[][] terrain, int row, int column){
     int count = 0;
     for( int rowOffset = -1; rowOffset <= 1; rowOffset++){
-      int wrapRow = Math.floorMod(row + rowOffset, terrain.length);
+      int wrapRow = (row + rowOffset + terrain.length) % terrain.length;
       for (int colOffset = -1; colOffset <= 1; colOffset++){
-        int wrapCol = Math.floorMod(column + colOffset, terrain[wrapRow].length);
+        int wrapCol = (column + colOffset + terrain[wrapRow].length) % terrain[wrapRow].length;
         if(terrain[wrapRow][wrapCol] == ALIVE){
           count++;
         }
