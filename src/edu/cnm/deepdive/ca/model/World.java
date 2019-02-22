9cf89006954fb148920f7c1888f2b368d07b84e9
Kevin Simms <ksimms2@cnm.edu>
2019-02-21 17:05:06 -0700

    added JAR artifact

--- src/edu/cnm/deepdive/ca/model/World.java
+++ src/edu/cnm/deepdive/ca/model/World.java
@@ -1,7 +1,10 @@
 package edu.cnm.deepdive.ca.model;
 
 import java.util.Arrays;
+import java.util.BitSet;
 import java.util.Random;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
 
 public class World {
 
@@ -11,18 +14,17 @@ public class World {
   private Cell[][] next;
   private long generation;
   private final Object lock = new Object();
+  private long checksum;
+  private int population;
 
-  public World(){
+  public World() {
     this(DEFAULT_SIZE);
-
-
   }
 
-  public World(int size){
-
+  public World(int size) {
     terrain = new Cell[size][size];
     next = new Cell[size][size];
-    for (Cell[] cellRow : terrain){
+    for (Cell[] cellRow : terrain) {
       Arrays.fill(cellRow, Cell.DEAD);
     }
   }
@@ -33,34 +35,53 @@ public class World {
       for (int col = 0; col < cellRow.length; col++) {
         if (rng.nextDouble() < threshold) {
           cellRow[col] = Cell.ALIVE;
+          population++;
         }
       }
     }
   }
-    public void set(int row, int col, Cell cell){
+
+  public void set(int row, int col, Cell cell) {
+    Cell previous = terrain[row][col];
+    if ((previous == null || previous == Cell.DEAD) && cell == Cell.ALIVE) {
+      population++;
+
+    } else if (previous == Cell.ALIVE && (cell == null || cell == Cell.DEAD)) {
+      population--;
+    }
     synchronized (lock) {
       terrain[row][col] = cell;
     }
-
   }
-  public void tick(){
-    for (int row = 0; row < terrain.length; row++){
-      for(int col = 0; col < terrain[row].length; col++){
+
+  public void tick() {
+    Checksum checksum = new CRC32();
+    BitSet bitSet = new BitSet(terrain[0].length);
+    int population = 0;
+    for (int row = 0; row < terrain.length; row++) {
+      for (int col = 0; col < terrain[row].length; col++) {
         next[row][col] = terrain[row][col].next(terrain, row, col);
-
+        bitSet.set(col, next[row][col] == Cell.ALIVE);
+        if (next[row][col] == Cell.ALIVE) {
+          population++;
+        }
       }
+      byte[] rowBytes = bitSet.toByteArray();
+      checksum.update(rowBytes, 0, rowBytes.length);
     }
+    this.checksum = checksum.getValue();
     synchronized (lock) {
-      for (int row = 0; row < terrain.length; row++){
+      for (int row = 0; row < terrain.length; row++) {
         System.arraycopy(next[row], 0, terrain[row], 0, next[row].length);
-
       }
       generation++;
+      this.population = population;
     }
   }
+
   public void copyTerrain(Cell[][] terrainCopy) {
     synchronized (lock) {
-      for(int row = 0; row < terrain.length; row++){
+      for (int row = 0; row < terrain.length; row++) {
         System.arraycopy(terrain[row], 0, terrainCopy[row], 0, terrain[row].length);
       }
     }
@@ -70,14 +91,22 @@ public class World {
     return generation;
   }
 
+  public long getChecksum() {
+    return checksum;
+  }
+
+  public int getPopulation() {
+    return population;
+  }
+
   @Override
   public String toString() {
     StringBuilder builder = new StringBuilder();
-    for (Cell [] cellRow : terrain){
-      for(Cell cellCol : cellRow){
-        if (cellCol == Cell.ALIVE){
-          builder.append("*");
-        }else{
+    for (Cell[] cellRow : terrain) {
+      for (Cell cellCol : cellRow) {
+        if (cellCol == Cell.ALIVE) {
+          builder.append('*');
+        } else {
           builder.append(' ');
         }
       }
@@ -85,4 +114,6 @@ public class World {
     }
     return builder.toString();
   }
+
 }
+
